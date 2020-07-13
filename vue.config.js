6d273ca9249c523e377ed49df9f2b1b3a8571f76
Jiang Wenjian <wenjian.jiang@foxmail.com>
2020-07-13 21:52:06 +0800

    fix icon path

--- vue.config.js
+++ vue.config.js
@@ -10,11 +10,11 @@ module.exports = {
     appleMobileWebAppStatusBarStyle: 'black',
 
     iconPaths: {
-      favicon32: '/icons/favicon-32x32.png',
-      favicon16: '/icons/favicon-16x16.png',
-      appleTouchIcon: '/icons/apple-touch-icon-152x152.png',
-      maskIcon: '/icons/safari-pinned-tab.svg',
-      msTileImage: '/icons/msapplication-icon-144x144.png'
+      favicon32: '/img/icons/favicon-32x32.png',
+      favicon16: '/img/icons/favicon-16x16.png',
+      appleTouchIcon: '/img/icons/apple-touch-icon-152x152.png',
+      maskIcon: '/img/icons/safari-pinned-tab.svg',
+      msTileImage: '/img/icons/msapplication-icon-144x144.png'
     }
   }
 }
